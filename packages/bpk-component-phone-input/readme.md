8a89cd70ca677a7805d2fc01cbc8d85f6ace31df
Tiago Engel <tiagohngl@gmail.com>
2018-05-30 11:03:51 +0100

    [BPK-1611] Add web phone input

---
+++ packages/bpk-component-phone-input/readme.md
@@ -0,0 +1,26 @@
+# bpk-component-phone-input
+
+> Backpack phone input component.
+
+## Installation
+
+```sh
+npm install bpk-component-phone-input --save-dev
+```
+
+## Usage
+
+```js
+import React from 'react';
+import BpkPhoneInput from 'bpk-component-phone-input';
+
+export default () => (
+  <BpkPhoneInput />
+)
+```
+
+## Props
+
+| Property              | PropType | Required | Default Value    |
+| --------------------- | -------- | -------- | ---------------- |
+| className             | string   | false    | null             |
---
+++ packages/bpk-component-phone-input/src/BpkNavigationBar-test.js
@@ -0,0 +1,105 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import BpkInput from 'bpk-component-input';
+import BpkSelect from 'bpk-component-select';
+import BpkPhoneInput from './BpkPhoneInput';
+
+const dialingCodeProps = {
+  id: 'dialing-code',
+  name: 'Dialing code',
+  className: 'dialing-code',
+};
+
+const dialingCodes = [
+  { code: '44', description: '+44' },
+  { code: '55', description: '+55' },
+];
+
+const defaultProps = {
+  id: 'phone-input-id',
+  name: 'Telephone input',
+  placeholder: 'Telephone number',
+  value: '1234',
+  dialingCode: '44',
+  className: 'fancy-input',
+  onChange: () => {},
+  dialingCodes,
+  dialingCodeProps,
+};
+
+describe('BpkPhoneInput', () => {
+  it('should render correctly', () => {
+    const tree = renderer.create(<BpkPhoneInput {...defaultProps} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render correctly with a "large" attribute', () => {
+    const tree = renderer
+      .create(<BpkPhoneInput {...defaultProps} large />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render correctly with a "disabled" attribute', () => {
+    const tree = renderer
+      .create(<BpkPhoneInput {...defaultProps} disabled />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render correctly with a "valid" attribute', () => {
+    const tree = renderer
+      .create(<BpkPhoneInput {...defaultProps} valid />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render correctly with a "containerProps" attribute', () => {
+    const tree = renderer
+      .create(
+        <BpkPhoneInput
+          {...defaultProps}
+          containerProps={{ className: 'container', 'aria-label': 'container' }}
+        />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should call "onChange" when phone number changes', () => {
+    const onChange = jest.fn();
+    const tree = shallow(
+      <BpkPhoneInput {...defaultProps} onChange={onChange} />,
+    );
+    tree.find(BpkInput).simulate('change', { target: { value: '99' } });
+    expect(onChange).toHaveBeenCalledWith(defaultProps.dialingCode, '99');
+  });
+
+  it('should call "onChange" when dialing code changes', () => {
+    const onChange = jest.fn();
+    const tree = shallow(
+      <BpkPhoneInput {...defaultProps} onChange={onChange} />,
+    );
+    tree.find(BpkSelect).simulate('change', { target: { value: '55' } });
+    expect(onChange).toHaveBeenCalledWith('55', defaultProps.value);
+  });
+});
